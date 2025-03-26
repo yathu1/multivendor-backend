ddccf56fc487f64ab61bde23087fc10a62659e9e
yathu1 <yathu2198@gmail.com>
2025-03-27 00:45:39 +0530

    category edit and delete

--- controllers/dasboard/categoryController.js
+++ controllers/dasboard/categoryController.js
@@ -94,8 +94,73 @@ class categoryController{
 
     // end method 
 
+    update_category = async (req, res) => {
+        const form = formidable()
+        form.parse(req, async(err,fields,files)=>{
+        if (err) {
+            responseReturn(res, 404,{ error : 'something went wrong'})
+        } else {
+            let {name} = fields
+            let {image} = files
+            const {id} = req.params;
+    
+            name = name.trim()
+            const slug = name.split(' ').join('-')
+    
+        try {
+            let result = null;
+            if (image) {
+                cloudinary.config({
+                    cloud_name: process.env.cloud_name,
+                    api_key: process.env.api_key,
+                    api_secret: process.env.api_secret,
+                    secure: true
+                });
+    
+                  result = await cloudinary.uploader.upload(image.filepath, { folder: 'categorys'})
+            }
+    
+            const updateData = {
+                name,
+                slug,
+            }
+    
+            if (result) {
+                updateData.image = result.url;
+            }
+            
+            const category = await categoryModel.findByIdAndUpdate(id,updateData, { new: true});
+            responseReturn(res, 200, {category, message: 'Category Updated successfully'})
+                    
+            } catch (error) {
+            responseReturn(res, 500,{ error : 'Internal Server Error'})
+            } 
+    
+                }
+                 
+            })
+        }
+    
+     // end method
 
-
+     deleteCategory = async (req, res) => {
+        try {
+            const categoryId = req.params.id;
+            const deleteCategory = await categoryModel.findByIdAndDelete(categoryId);
+    
+            if (!deleteCategory) {
+                console.log(`Cateogry with id ${categoryId} not found`);
+                return res.status(404).json({ message: 'Category not found'});
+            }
+            res.status(200).json({message: 'Category deleted successfully'});
+            
+        } catch (error) {
+            console.log(`Error delete category with id ${categoryId}:`,error);
+            res.status(500).json({message: 'Internal Server Error'});
+        }
+    
+     }
+     // end method
 
 }
  
